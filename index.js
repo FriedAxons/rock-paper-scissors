1a4d61944a7a89302ab6c513ed2fda8451fa749d
FriedAxons <remo453@gmail.com>
2023-06-29 02:30:53 -0400

    Begin implementing button UI for game

--- index.js
+++ index.js
@@ -1,70 +1,80 @@
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * 3);
-  return choices[randomIndex];
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
 }
 
 function playRound(playerSelection, computerSelection) {
-  // Create if statement to handle the different conditions of the selections made by the computer and the player
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
-  ) {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-  }
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    // Prompt player for their choice
-    let playerSelection = prompt(
-      "Enter your choice (rock, paper, or scissors):"
-    );
-
-    // Check if player's input is valid
-    while (
-      !(
-        playerSelection === "rock" ||
-        playerSelection === "paper" ||
-        playerSelection === "scissors"
-      )
+    // Create if statement to handle the different conditions of the selections made by the computer and the player
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
     ) {
-      playerSelection = prompt(
-        "Invalid input. Please choose rock, paper, or scissors"
-      );
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
-
-    // Generate computer's move
-    let computerSelection = getComputerChoice();
-
-    let result = playRound(playerSelection.toLowerCase(), computerSelection);
-    console.log(result);
-
-    if (result.includes("win")) {
-      playerScore++;
-    } else if (result.includes("lose")) {
-      computerScore++;
-    }
-  }
-
-  console.log(`Player score: ${playerScore}`);
-  console.log(`Computer score: ${computerScore}`);
-
-  if (playerScore > computerScore) {
-    console.log("You win the game!");
-  } else if (computerScore > playerScore) {
-    console.log("Computer wins the game!");
-  } else {
-    console.log("It's a tie game!");
-  }
 }
 
-game();
+// Get references to the UI elements
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
+
+// Add event listeners to the buttons
+rockBtn.addEventListener("click", () => playGame("rock"));
+paperBtn.addEventListener("click", () => playGame("paper"));
+scissorsBtn.addEventListener("click", () => playGame("scissors"));
+
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+
+//   for (let i = 0; i < 5; i++) {
+//     // Prompt player for their choice
+//     let playerSelection = prompt(
+//       "Enter your choice (rock, paper, or scissors):"
+//     );
+
+//     // Check if player's input is valid
+//     while (
+//       !(
+//         playerSelection === "rock" ||
+//         playerSelection === "paper" ||
+//         playerSelection === "scissors"
+//       )
+//     ) {
+//       playerSelection = prompt(
+//         "Invalid input. Please choose rock, paper, or scissors"
+//       );
+//     }
+
+//     // Generate computer's move
+//     let computerSelection = getComputerChoice();
+
+//     let result = playRound(playerSelection.toLowerCase(), computerSelection);
+//     console.log(result);
+
+//     if (result.includes("win")) {
+//       playerScore++;
+//     } else if (result.includes("lose")) {
+//       computerScore++;
+//     }
+//   }
+
+//   console.log(`Player score: ${playerScore}`);
+//   console.log(`Computer score: ${computerScore}`);
+
+//   if (playerScore > computerScore) {
+//     console.log("You win the game!");
+//   } else if (computerScore > playerScore) {
+//     console.log("Computer wins the game!");
+//   } else {
+//     console.log("It's a tie game!");
+//   }
+// }
+
+// game();
